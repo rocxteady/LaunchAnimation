c41fd9a6d38a8ed39d3771b5657062dc5a6c4be4
Ulaş Sancak <ulas.sancak@hotmail.com.tr>
2024-10-05 15:24:17 +0300

    Initial Commit

---
+++ LaunchAnimation/LaunchAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchAnimationApp.swift
+//  LaunchAnimation
+//
+//  Created by Ulaş Sancak on 5.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LaunchAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
